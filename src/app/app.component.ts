819b030dcfe6c8b8de36eac7ccafa14bbc2cd0eb
Nico Arato <nico.arato@gmail.com>
2020-02-09 18:43:03 -0300

    More pipes.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,29 @@ export class AppComponent {
   title = 'pipes';
   nombre = "Fernando";
   arreglo= [1,2,3,4,5,6,7,8,9,10];
+
+  PI = Math.PI;
+
+  a = 0.234;
+
+  salario = 1234.5;
+
+  heroe = {
+    nombre: "Logan",
+    clave: "Wolverine",
+    edad: 500,
+    direccion:{
+      calle: "Primera",
+      casa: "19"
+    }
+  };
+
+  valorDePromesa = new Promise( (resolve, reject) => {
+    setTimeout( () => resolve('Llegó la data'), 3500);
+  });
+
+  fecha = new Date();
+
+  nombre2 = "antonia RupErta López";
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, enableProdMode } from '@angular/core';
+import { LOCALE_ID} from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CapitalizadoPipe } from './pipes/capitalizado.pipe';
+
+import localeEs from '@angular/common/locales/es';
+import { registerLocaleData } from '@angular/common';
+registerLocaleData(localeEs);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CapitalizadoPipe
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: "es"}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
